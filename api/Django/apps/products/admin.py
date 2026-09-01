3f77abc529604881368455c2b4273872a0f5c8d8
amir <amirmad1385@gmail.com>
2026-09-01 15:44:38 +0330

    add initial Locust load test

--- api/Django/apps/products/admin.py
+++ api/Django/apps/products/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Category
-from .models import Products
-
+from .models import Product
 
 
 
 admin.site.register(Category)
-admin.site.register(Products)
+admin.site.register(Product)
--- api/Django/apps/products/models.py
+++ api/Django/apps/products/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 
-
 class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
